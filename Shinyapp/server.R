e278e71230d3e3f7175bb6a4c1f782e3cc83d02c
MITHUN KUMAR <mithunkumar039@gmail.com>
2018-03-28 14:20:18 +0530

    Update server.R

--- Shinyapp/server.R
+++ Shinyapp/server.R
@@ -1,19 +1,13 @@
-
-
-
 library(shiny)
 
 shinyServer(function(input, output) {
 
-  output$distPlot <- renderPlot({
-
-    
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-    
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
-  })
+  output$sum <-renderPrint(
+    summary(insurance)
+  ) 
 
 })
+
+
+
+
